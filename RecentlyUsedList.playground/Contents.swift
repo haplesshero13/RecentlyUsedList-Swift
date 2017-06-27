bfd89334b72090de7c0d4084a37769112120c774
Avery Yen <ayen@pivotal.io>
2017-06-27 17:00:10 -0400

    Sample solution.
    Signed-off-by: Amanda Munoz <amunoz@pivotal.io>

--- RecentlyUsedList.playground/Contents.swift
+++ RecentlyUsedList.playground/Contents.swift
@@ -1,11 +1,36 @@
 import XCTest
 
 class RecentlyUsedListTests: XCTestCase {
+    var list: RecentlyUsedList!
+
     override func setUp() {
         super.setUp()
+        list = RecentlyUsedList()
     }
-    
-    func testList() {
+
+    func testListStartsEmpty() {
+        XCTAssertEqual(list.size, 0)
+    }
+
+    func testInsert() {
+        list.insert("first element")
+        list.insert("second element")
+
+        XCTAssertEqual(list.size, 2)
+        XCTAssertEqual(list[0], "second element")
+        XCTAssertEqual(list[1], "first element")
+    }
+
+    func testInsertExistingElement() {
+        list.insert("first element")
+        list.insert("second element")
+        list.insert("third element")
+        list.insert("first element")
+
+        XCTAssertEqual(list[0], "first element")
+        XCTAssertEqual(list[1], "third element")
+        XCTAssertEqual(list[2], "second element")
+        XCTAssertEqual(list.size, 3)
     }
 }
 
--- RecentlyUsedList.playground/Sources/RecentlyUsedList.swift
+++ RecentlyUsedList.playground/Sources/RecentlyUsedList.swift
@@ -1,5 +1,22 @@
 import Foundation
 
-public struct RecentlyUsedList {
+public class RecentlyUsedList {
+    private var elements = [String]()
+
     public init() {}
+
+    public var size: Int {
+        get { return elements.count }
+    }
+
+    public subscript(index: Int) -> String {
+        get { return elements[index] }
+    }
+
+    public func insert(_ element: String) {
+        if let currentIndex = elements.index(of: element) {
+            elements.remove(at: currentIndex)
+        }
+        elements.insert(element, at: 0)
+    }
 }
